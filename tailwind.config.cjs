48426e84b4e7b0a2c65be6cb45672ee0435597b4
Bjørn Fridal <bjorn@fridal.dk>
2023-10-03 22:26:32 +0200

    feat: Tweak blog layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: [],
   theme: {
     extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
+            color: theme('colors.black'),
+
             h4: {
               fontWeight: 'normal',
               fontSize: '1.25rem',
